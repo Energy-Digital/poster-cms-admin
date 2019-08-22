764642d5c946a1c40cc1dee75e350d7b87c78314
JeffWu <isjeffcom@gmail.com>
2019-08-22 17:19:37 +0800

    登录返回请求key，使用请求key请求所有up接口，写入cookie完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -2,7 +2,7 @@
     <div id="all">
         <el-input v-model="email" placeholder="Email Account"></el-input>
         <br>
-        <el-input v-model="psw" placeholder="Passcode" type="number"></el-input>
+        <el-input v-model="psw" placeholder="Passcode" type="password"></el-input>
         <br>
         <el-button type="primary" v-on:click="submit">LOGIN</el-button>
     </div>
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+
+
 export default {
     name:"login",
     props: {
@@ -47,9 +49,17 @@ export default {
                 .then(function (response) {
 
                     var res = response.data
-                    console.log(res)
 
-                    if(res.indexOf("success")){
+                    if(res.indexOf("success") != -1){
+
+                        var sucInfo = res.split(',')
+
+                        that.clearCookie()
+
+                        that.setCookie('u_key',sucInfo[1], 30, false) // Set Key
+                        that.setCookie('u_uuid',sucInfo[2], 30, false) // Set UUID
+                        that.setCookie('u_email',that.email, 30, false) // Set UEmail
+                        
                         that.$notify({
                             title: '验证成功',
                             message: '已完成验证，返回成功',
@@ -68,6 +78,26 @@ export default {
 
                 
             }
+        },
+
+        setCookie (name, value, expDays, remove) {
+            var exp, expTime
+            
+            if(!remove){
+                expTime = (((60 * 1000)*60)*24)*expDays // 30 days
+                exp = new Date()
+                exp.setTime(exp.getTime() + expTime)
+                document.cookie = name + "=" + value + ";" + "expires=" + exp.toGMTString()+";"
+            } else {
+                document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+            }
+            
+        },
+
+        clearCookie () {
+            this.setCookie('u_key', 0, 30, true) // Set Key
+            this.setCookie('u_uuid', 0, 30, true) // Set UUID
+            this.setCookie('u_email', 0, 30, true) // Set UEmail
         }
     }
 }
