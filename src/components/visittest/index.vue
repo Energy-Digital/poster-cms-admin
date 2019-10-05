ec5f2d79cf667896e28b20c66154729fe9ce2c17
JeffWu <isjeffcom@gmail.com>
2019-10-05 22:41:52 +0100

    优化用户追踪，优化主题设置支持file字段， clean

--- src/components/visittest/index.vue
+++ src/components/visittest/index.vue
@@ -41,80 +41,8 @@ export default {
   },
   methods:{
 
-    setTracker () {
-        var tmpUid = parseInt(Math.ceil(Math.random()*1000) * Date.parse( new Date()) / 10000000)
-        setCookieExInMin('isjeffcom', tmpUid, 1)
-    },
-
     getIp () {
-
-        if(getCookie('isjeffcom')){
-            return
-        }
-
-        var that = this
-
-        this.axios.get(this.base + this.api_ip).then((response) => {
-
-          if(response.data.result == false){
-            console.log("rec fail")
-            return
-          }
-
-          that.userInfoRes.ip = response.data
-          that.userInfoRes.userAgent = navigator.userAgent
-          that.userInfoRes.userLanguage = navigator.language
-          that.userInfoRes.appName = navigator.appName
-          that.userInfoRes.platform = navigator.platform
-
-
-          if(response.data.length > 1){
-
-              that.axios.get(that.api_getIpInfo + response.data + that.api_key).then((response)=>{
-                var res = response.data
-
-
-                that.userInfoRes.ipCountryCode = res.country_code
-                that.userInfoRes.ipCountry = res.country_name
-                that.userInfoRes.geoLocation = res.latitude + ',' + res.longitude
-                that.userInfoRes.flagIcon = res.location.country_flag
-                that.userInfoRes.isAdmin = 0
-
-                var postReady = {
-                    ukey: getCookie('u_key'), 
-                    uuid: getCookie('u_uuid'), 
-                }
-
-                postReady = Object.assign(that.userInfoRes, postReady)
-                var postData = that.$qs.stringify(postReady)
-
-                that.axios.post(that.base + that.api_up, postData)
-                .then(function (response) {
-                  //console.log(response.data)
-                    if(response.data.indexOf("success") != -1){
-                      console.log('Visit Loged')
-                    }
-                })
-
-                //that.setTracker()
-
-              })
-
-          } else {
-            // Try 10 times
-              /*that.axios.post(that.api_up, postData)
-              .then(function (response) {
-                //console.log(response.data)
-                  if(response.data.indexOf("success") != -1){
-                    console.log('Visit Loged')
-                  }
-              })*/
-
-              //that.setTracker()
-          }
-        })
-
-      
+      logVisit(this.base + this.api_up)
     },
 
 
